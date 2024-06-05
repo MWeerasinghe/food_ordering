241c49b0acb877c7f0715bb6eb7b35801ddef7e8
mithun <mithunweerasinghe999@gmail.com>
2024-06-05 13:11:55 +0530

    starging

---
+++ src/test/java/com/mithun/OnlineFoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mithun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
