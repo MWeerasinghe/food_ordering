50805dfdfc2dd431d028d1da075acb3c94c6572c
mithun <mithunweerasinghe999@gmail.com>
2024-07-06 13:34:33 +0530

    finished CustomerUserDetailService

---
+++ src/main/java/com/mithun/service/CustomerUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.mithun.service;
+
+import com.mithun.model.USER_ROLE;
+import com.mithun.model.User;
+import com.mithun.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Because of this function it doesn't want to generate auto password
+@Service
+public class CustomerUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        User user = userRepository.findByEmail(username);
+
+        if(user!=null){
+            throw new UsernameNotFoundException("user not found with email "+username);
+        }
+
+        USER_ROLE role = user.getRole();
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority(role.toString()));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),authorities);
+    }
+}
