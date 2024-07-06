02e0dddbf35130b88fa47c54c1e2765c80741e34
mithun <mithunweerasinghe999@gmail.com>
2024-07-06 12:45:13 +0530

    finished security config and checking for any errors

--- src/main/java/com/mithun/config/AppConfig.java
+++ src/main/java/com/mithun/config/AppConfig.java
@@ -1,11 +1,20 @@
 package com.mithun.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 //for security configuration
 @Configuration
@@ -15,11 +24,42 @@ public class AppConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(Authorize -> Authorize
-                        .requestMatchers("/api/admin/**").hasAnyRole("RESTAURAN-OWNER","ADMIN") //give access to those roles
+                        .requestMatchers("/api/admin/**").hasAnyRole("RESTAURANT_OWNER","ADMIN") //give access to those roles
                         .requestMatchers("/api?**").authenticated() //should proved JWD token
                         .anyRequest().permitAll() //no roll or jwd tokens are needed for singup and sign in
-                )
+                ).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf->csrf.disable())
+                .cors(cors->cors.configurationSource(corsConfigrationSource()));
+        
+        return null;
+        
+    }
 
+    private CorsConfigurationSource corsConfigrationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg = new CorsConfiguration();
 
+                cfg.setAllowedOrigins(Arrays.asList(
+                        "https://mithun-food.vercel.app",
+                        "http://localhost:5454"
+                ));
+
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setExposedHeaders(Arrays.asList("Authorization"));
+                cfg.setMaxAge(3600L);
+
+                return cfg;
+            }
+        };
+    }
+
+    //like hashing
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/mithun/config/JwtConstant.java
@@ -0,0 +1,9 @@
+package com.mithun.config;
+
+public class JwtConstant {
+
+    public static final String SECRETE_KEY="skldjfslkdjajfalksjfheuifnvskdnvsuehfdjfsfndskheihvnesvinisdeivnsdkf";
+
+    public static final String JWT_HEADER="Authorization";
+
+}
